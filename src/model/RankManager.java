115d8e9f6d497f2010e3d81bf66e2b00cdf26188
咒之师 <18163520552@163.com>
2025-05-20 17:56:09 +0800

    修复了一些小bug，项目基本完成

--- src/model/RankManager.java
+++ src/model/RankManager.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class RankManager {
-    private static final int MAX_ARRAY = 20;
+    private static final int MAX_ARRAY = 5;
 
     public static void saveRankData(String levelName, Rank rank) {
         String path = Constants.RANK_DIRECTORY +  levelName + ".rank";
